056865f6487b3864d6cc6fb03633e5a9cb36d071
Hieu087 <46962064+Hieu087@users.noreply.github.com>
2020-01-14 14:31:43 +0700

    Update 14/1

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
-    //------------------ACTIVE ICON------------------
+    //------------------ACTIVE ICON & PANEL------------------
     $('.icon').click(
         function(){
             $('.icon').removeClass('active');
             $(this).toggleClass('active');
+
+            if($(this).attr('id') == 'home-icon'){
+                $('.panel').removeClass('appear');
+                $('#home').addClass('appear');
+            }
+            if($(this).attr('id') == 'projects-icon'){
+                $('.panel').removeClass('appear');
+                $('#projects').addClass('appear');
+            }
+            if($(this).attr('id') == 'contact-icon'){
+                $('.panel').removeClass('appear');
+                $('#contact').addClass('appear');
+            }
+
         }
     )
 })
